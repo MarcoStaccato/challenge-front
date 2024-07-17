2db47af996f29d1c36da9b7184b1841f621cf1f5
Marco Martinez <marcostaccato@gmail.com>
2024-07-17 01:18:14 -0600

    run operations

--- src/components/Operation.vue
+++ src/components/Operation.vue
@@ -5,33 +5,82 @@
       <div class="col-3">
         <n-space vertical>
           <n-card title="Operations" :bordered="false"></n-card>
-          <n-card title="Addition" @click="compute('addition')" size="small" hoverable></n-card>
-          <n-card title="Substraction" @click="compute('substraction')" size="small" hoverable></n-card>
-          <n-card title="Multiplication" @click="compute('multiplication')" size="small" hoverable></n-card>
-          <n-card title="Division" @click="compute('division')" size="small" hoverable></n-card>
-          <n-card title="Square Root" @click="compute('sqrt')" size="small" hoverable></n-card>
-          <n-card title="Random String" @click="compute('random')" size="small" hoverable></n-card>
+          <n-card title="Addition" @click="setOperation('addition')" size="small" hoverable></n-card>
+          <n-card title="Substraction" @click="setOperation('substraction')" size="small" hoverable></n-card>
+          <n-card title="Multiplication" @click="setOperation('multiplication')" size="small" hoverable></n-card>
+          <n-card title="Division" @click="setOperation('division')" size="small" hoverable></n-card>
+          <n-card title="Square Root" @click="setOperation('sqrt')" size="small" hoverable></n-card>
+          <n-card title="Random String" @click="setOperation('random')" size="small" hoverable></n-card>
         </n-space>
       </div>
-      <div class="col-8">test</div>
+      <div class="col-9" style="margin-top: 15%;">
+        <n-space v-if="operator !== 'random' && operator !== 'sqrt' " justify="left">
+          <n-button strong secondary round type="info" @click="compute()">
+            Run Operation
+          </n-button>
+          <br style="display: block;">
+          <n-input ref="numeratorId"
+            type="textarea"
+            :allow-input="onlyAllowNumber"
+            placeholder="Numerator"
+            @input="handleNum"
+          />
+          <h3 class="result">{{this.symbol}}</h3>
+          <n-input ref="denominatorId"
+            type="textarea"
+            :allow-input="onlyAllowNumber"
+            placeholder="Denominator"
+            @input="handleDenom"
+          />
+          <h3 class="result">=</h3>
+          <h3 class="result">{{this.result}}</h3>
+        </n-space>
+        <n-space v-if="operator === 'random'" justify="left">
+          <n-button strong secondary round type="info" @click="compute()">
+            Compute
+          </n-button>
+          <h3 class="result" style="display: block;"> Random String: </h3>
+          <h3 class="result">{{this.result}}</h3>
+        </n-space>
+        <n-space vertical>
+          
+            
+        </n-space>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { NCard, NSpace } from 'naive-ui'
+import { NCard, NSpace, NInput, NButton } from 'naive-ui'
 import OpsService from '../services/ops.service';
+
 
 export default {
   name: 'Profile',
   components: {
-      NCard,
-      NSpace
-    },
+    NCard,
+    NSpace,
+    NInput,
+    NButton
+  },
+  data() {
+    return {
+      numerator: 0,
+      denominator: 0,
+      operator: "addition",
+      result: 0,
+      symbol: "+"
+    }
+  },
   computed: {
     currentUser() {
       return this.$store.state.auth.user;
+    },
+    dataResult: function () {
+      return this.result;
     }
+
   },
   mounted() {
     if (!this.currentUser) {
@@ -39,9 +88,28 @@ export default {
     }
   },
   methods: {
-    compute(operation) {
-      OpsService.compute(1, 1, operation);
+    compute() {
+      OpsService.compute(this.$data.numerator, this.$data.denominator).then((value) => {
+      this.result = value.data
+
+      // Expected output: "Success!"
+    });
     },
+    onlyAllowNumber: (value) => !value || /^\d+$/.test(value),
+    setOperation(op){
+      localStorage.setItem('operation', op)
+      this.$data.operator = op;
+      this.$data.symbol = OpsService.getSymbol(op);
+
+      this.$data.result = "";
+
+    },
+    handleNum(v) {
+      this.$data.numerator = v 
+    },
+    handleDenom(v) {
+      this.$data.denominator = v
+    }
   },
 };
 </script>
--- src/services/ops.service.js
+++ src/services/ops.service.js
@@ -3,21 +3,69 @@ import axios from 'axios';
 const API_URL = 'http://localhost.elpalomito.io:8080/api/v1/operations/';
 
 class OpsService {
-  compute(operator, numerator, operation) {
+  compute(numerator, denominator) {
+
+    const storedOp = localStorage.getItem('operation');
+
+    var computeOp = "RANDOM_STRING";
+
+    switch (storedOp) {
+      case "addition":
+        computeOp = "ADDITION"
+        break;
+      case "substraction":
+        computeOp = "SUBSTRACTION"
+        break;
+      case "division":
+        computeOp = "DIVISION"
+        break;
+      case "multiplication":
+        computeOp = "MULTIPLICATION"
+        break;
+      case "sqrt":
+        computeOp = "SQUARE_ROOT"
+        break;
+      case "random":
+        computeOp = "RANDOM_STRING"
+        break;
+      default:
+        //can't process that operation
+        return;
+    }
+
     return axios.post(API_URL + 'compute', {
-        operator: operator,
-        numerator: numerator,
-        operation: operation
+        operator: numerator,
+        numerator: denominator,
+        operation: computeOp
       },
       {
         headers: {
-            'Authorization': localStorage.getItem('authorization'),
-            'MyCustomHeader2': '2'
+            'Authorization': localStorage.getItem('authorization')
         }
     })
       .then(response => {
           return response;
       });
+  }
+
+  getSymbol(operation) {
+    switch (operation) {
+      case "addition":
+        return "+";
+      case "substraction":
+        return "-";
+      case "division":
+        return "/"
+      case "multiplication":
+        return "x"
+      case "sqrt":
+        return "sqrt"
+      case "random":
+        return "random"
+      default:
+        //can't process that operation
+        return ""
+    }
   }
 }
 
